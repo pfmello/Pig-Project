3b547ecbba66c6a2e0b5b3abb5e9e8dbd88dfbbd
PFMello <philipmikh@gmail.com>
2023-11-08 18:29:39 -0300

    logica do game parcialmente finalizada

--- script.js
+++ script.js
@@ -6,29 +6,43 @@ const btnDiceRoll = document.querySelector('.btn--roll');
 const player0 = document.querySelector('.player--0');
 const player1 = document.querySelector('.player--1');
 
+const btnHold = document.querySelector('.btn--hold');
+
+player0.points = 5;
+player1.points = 5;
+
+player0.score = 0;
+player1.score = 0;
+
 let currentPlayer = player0;
-currentPlayer.score = 20;
+
+const changePlayer = function () {
+  currentPlayer.classList.toggle('player--active');
+
+  currentPlayer =
+    currentPlayer === player0
+      ? (currentPlayer = player1)
+      : (currentPlayer = player0);
+
+  currentPlayer.classList.toggle('player--active');
+};
 
 const diceRoll = function () {
   let diceNumber = Math.floor(Math.random() * 6) + 1;
 
   switch (diceNumber) {
     case 1:
+      currentPlayer.points = 0;
+      currentPlayer.querySelector('.current-score').textContent =
+        currentPlayer.points;
       dice.src = 'dice-1.png';
-      currentPlayer.classList.toggle('player--active');
-
-      currentPlayer =
-        currentPlayer === player0
-          ? (currentPlayer = player1)
-          : (currentPlayer = player0);
-
-      currentPlayer.classList.toggle('player--active');
+      changePlayer();
 
       break;
     case 2:
-      currentPlayer.score += 2;
+      currentPlayer.points += 2;
       currentPlayer.querySelector('.current-score').textContent =
-        currentPlayer.score;
+        currentPlayer.points;
       dice.src = 'dice-2.png';
       break;
     case 3:
@@ -49,3 +63,10 @@ const diceRoll = function () {
 };
 
 btnDiceRoll.addEventListener('click', diceRoll);
+
+btnHold.addEventListener('click', function () {
+  currentPlayer.score += currentPlayer.points;
+  currentPlayer.querySelector('.score').textContent = currentPlayer.score;
+
+  changePlayer();
+});
