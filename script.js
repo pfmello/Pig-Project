c502dcd4babf73874925c234d1a59818220c90ac
PFMello <philipmikh@gmail.com>
2023-11-09 13:30:01 -0300

    melhorado a logica do game

--- script.js
+++ script.js
@@ -5,78 +5,74 @@ const score0Element = document.getElementById('score--0');
 const score1Element = document.getElementById('score--1');
 const diceElement = document.querySelector('.dice');
 
+// Botões new game, roll dice e hold
+const btnNewGame = document.querySelector('.btn--new');
+const btnDiceRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
 // Condições de início de jogo
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
 
-const btnDiceRoll = document.querySelector('.btn--roll');
-
 const player0 = document.querySelector('.player--0');
 const player1 = document.querySelector('.player--1');
 
-const btnHold = document.querySelector('.btn--hold');
+player0.currentScore = 0;
+player1.currentScore = 0;
 
-player0.points = 5;
-player1.points = 5;
+let currentScore = 0;
 
 player0.score = 0;
 player1.score = 0;
 
-let currentPlayer = player0;
+let activePlayer = player0;
 
 const changePlayer = function () {
-  currentPlayer.classList.toggle('player--active');
+  activePlayer.classList.toggle('player--active');
 
-  currentPlayer =
-    currentPlayer === player0
-      ? (currentPlayer = player1)
-      : (currentPlayer = player0);
+  activePlayer =
+    activePlayer === player0
+      ? (activePlayer = player1)
+      : (activePlayer = player0);
 
-  currentPlayer.classList.toggle('player--active');
+  activePlayer.classList.toggle('player--active');
 };
 
-const diceRoll = function () {
+btnDiceRoll.addEventListener('click', function () {
+  // Gerando o número do dado
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+
+  // Mostrando o dado na tela
   diceElement.classList.remove('hidden');
-  let diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceElement.src = `dice-${diceNumber}.png`;
 
-  switch (diceNumber) {
-    case 1:
-      currentPlayer.points = 0;
-      currentPlayer.querySelector('.current-score').textContent =
-        currentPlayer.points;
-      diceElement.src = 'dice-1.png';
-      changePlayer();
+  // Se o dado for 1, muda de player e reseta os pontos
+  if (diceNumber === 1) {
+    activePlayer.currentScore = 0;
+    activePlayer.querySelector('.current-score').textContent =
+      activePlayer.currentScore;
+    changePlayer();
+  } else {
+    currentScore += diceNumber;
 
-      break;
-    case 2:
-      currentPlayer.points += 2;
-      currentPlayer.querySelector('.current-score').textContent =
-        currentPlayer.points;
-      diceElement.src = 'dice-2.png';
-      break;
-    case 3:
-      diceElement.src = 'dice-3.png';
-      break;
-    case 4:
-      diceElement.src = 'dice-4.png';
-      break;
-    case 5:
-      diceElement.src = 'dice-5.png';
-      break;
-    case 6:
-      diceElement.src = 'dice-6.png';
-      break;
-    default:
-      break;
+    activePlayer.currentScore += diceNumber;
+    activePlayer.querySelector('.current-score').textContent =
+      activePlayer.currentScore;
   }
-};
-
-btnDiceRoll.addEventListener('click', diceRoll);
+});
 
 btnHold.addEventListener('click', function () {
-  currentPlayer.score += currentPlayer.points;
-  currentPlayer.querySelector('.score').textContent = currentPlayer.score;
+  activePlayer.score += activePlayer.currentScore;
+  activePlayer.currentScore = 0;
+  activePlayer.querySelector('.current-score').textContent =
+    activePlayer.currentScore;
+
+  activePlayer.querySelector('.score').textContent = activePlayer.score;
+
+  if (activePlayer.score >= 100) {
+    alert('venceuu !');
+  }
 
   changePlayer();
 });
